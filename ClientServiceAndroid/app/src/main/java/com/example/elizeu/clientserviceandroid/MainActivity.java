6ca0ad6b777ab8e5e77c74279216174663829630
Elizeu Castro <elizeuvoraf@gmail.com>
2015-08-28 00:52:30 -0400

    adicionando opção de configuração do ip

--- ClientServiceAndroid/app/src/main/java/com/example/elizeu/clientserviceandroid/MainActivity.java
+++ ClientServiceAndroid/app/src/main/java/com/example/elizeu/clientserviceandroid/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.elizeu.clientserviceandroid;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,28 +15,51 @@ public class MainActivity extends Activity implements ServiceManager.ServiceCall
 
     private ServiceManager serviceManager = new ServiceManager();
     private ListView listView;
+    private EditText edtIp;
+    private Button btnRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+
         listView = (ListView) findViewById(R.id.lv_products);
-        findViewById(R.id.btn_refresh).setOnClickListener(new View.OnClickListener() {
+        edtIp = (EditText) findViewById(R.id.edt_ip);
+        btnRefresh = (Button) findViewById(R.id.btn_refresh);
+
+        edtIp.setText(getLastIp());
+
+        btnRefresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                serviceManager.getProducts(MainActivity.this);
+                String serverIp = edtIp.getText().toString().trim();
+                if (!serverIp.isEmpty()) {
+                    saveLastIp(serverIp);
+                    serviceManager.setServerIp(serverIp);
+                    serviceManager.getProducts(MainActivity.this);
+                }
             }
         });
+    }
 
+    private String getLastIp() {
+        SharedPreferences preferences = getSharedPreferences("CLIENT_ANDROID", 0);
+        String lastIP = preferences.getString("LAST_IP", "");
+        return lastIP;
+    }
+
+    private void saveLastIp(String serverIp){
+        SharedPreferences preferences = getSharedPreferences("CLIENT_ANDROID", 0);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("LAST_IP", serverIp).commit();
     }
 
     @Override
     public void onServiceSuccess(List<Produtos> products) {
-        if (products.isEmpty()){
+        ProductsAdapter adapter = new ProductsAdapter(products);
+        listView.setAdapter(adapter);
+        if (products.isEmpty()) {
             Toast.makeText(this, "Não há produtos registrados", Toast.LENGTH_LONG).show();
-        } else {
-            ProductsAdapter adapter = new ProductsAdapter(products);
-            listView.setAdapter(adapter);
         }
     }
 
